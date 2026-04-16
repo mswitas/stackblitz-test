02431fad32de80003c91c0e7a77b31adacab1d27
mswitas <git@stackblitz.com>
2026-04-16 10:25:29 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <p>It's working :D</p>
+    </>
+  )
+}
+
+export default App
